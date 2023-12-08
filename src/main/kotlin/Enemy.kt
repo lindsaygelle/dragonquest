51f2a4d308c1e987317fad14c1746d33ca363281
Linneeeeeeee <lindsay.gelle@gmail.com>
2023-12-08 16:07:06 +1100

    Dev

--- src/main/kotlin/Enemy.kt
+++ src/main/kotlin/Enemy.kt
@@ -1,49 +1,36 @@
-import kotlin.random.Random
-
 public open class Enemy(
-    agilityMaximum: Int,
-    experiencePointsMaximum: Int,
-    goldPointsMaximum: Int,
-    hitPointsMaximum: Int,
-    magicPointsMaximum: Int,
-    name: String,
-    public final val statusResistanceMaximum: Int,
-    strengthMaximum: Int,
-) : Character(
-    agilityMaximum,
-    Actor.Category.NPC,
-    experiencePointsMaximum,
-    goldPointsMaximum,
-    // Dragonlord can always have the maximum hit points. Everyone else is scaled.
-    // This feels kinda clunky to have here. But it allows me to keep Character clean.
-    if (name == "Dragonlord") hitPointsMaximum else hitPointsMaximum - ((Random.nextInt(
-        0,
-        256
-    ) * hitPointsMaximum) shr 10),
-    magicPointsMaximum,
-    name,
-    strengthMaximum
-) {
-    public final override val agility: Int
-        // Calculate agility to be currently the max.
-        // Could implement scaling and changes based on battle conditions.
-        get() = agilityMaximum
+    agility: Int,
+    damageResistance: Int,
+    hitPoints: Int,
+    magicPoints: Int,
+    public override final var name: String,
+    statusResistance: Int,
+    strength: Int,
+) :
+    Character(
+        agility = minOf(255, maxOf(3, agility)),
+        hitPoints = minOf(160, maxOf(3, hitPoints)),
+        magicPoints = magicPoints,
+        name = name,
+        strength = minOf(140, maxOf(5, strength)),
+    ) {
 
-    // Calculate using gold scaling formula. Replace when experience formula is found.
-    public final val experienceReward: Int =
-        (((Random.nextInt(0, 64) + 0xC0) * experiencePointsMaximum) / 256) + 1
+    public final override val attackScore: Int
+        get() = strength
 
+    public final var damageResistance: Int = maxOf(0, minOf(5, damageResistance))
+    public final override val defenseScore: Int
+        get() = (agility / 2)
 
-    // goldReward uses the Dragon Quest gold formula (GP * ((0..63 & 0x3F) + 0xC0)) >> 8
-    // Add 1 for some reward? Could toggle this as some difficulty scaling thing.
-    public final val goldReward: Int =
-        (((Random.nextInt(0, 64) + 0xC0) * goldPointsMaximum) / 256) + 1
+    public final val hurtResistance: Int
 
-    public final override val sleepResistance: Int =
-        (statusResistanceMaximum shr 4) and 0xF // Get first nibble (nybble) of statusResistanceMaximum.
+    public final val sleepResistance: Int
+    public final val statusResistance: Int = maxOf(0, minOf(255, statusResistance))
+    public final val stopSpellResistance: Int
 
-    public final override val strength: Int
-        // Calculate strength to be currently the max.
-        // Could implement scaling and changes based on battle conditions.
-        get() = strengthMaximum
+    init {
+        hurtResistance = (this.damageResistance and 0xF)
+        sleepResistance = (this.statusResistance shr 4) and 0xF
+        stopSpellResistance = (this.statusResistance and 0xF)
+    }
 }
